ecd48de9da214dcc58c7045f388494d9dd9355af
codewithoutfarhan <fparwez83@gmail.com>
2026-01-31 13:55:41 +0530

    Update Wave_Array.java
    Added a simple implementation of the Wave Array problem.
    
    Approach:
    - Traverse the array in steps of two.
    - Swap adjacent elements to create a wave-like pattern.
    
    Note:
    This solution focuses on understanding adjacent swaps
    and does not perform sorting.
    
    Language: Java
    Platform: GeeksforGeeks

--- Wave_Array.java
+++ Wave_Array.java
@@ -14,3 +14,21 @@ class Solution {
         }
     }
 }
+
+
+  // second possible approach 
+
+class Solution {
+    public void sortInWave(int arr[]) {
+
+        for (int i = 0; i < arr.length; i += 2) {
+
+            if (i == arr.length - 1)
+                break;
+
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+}
